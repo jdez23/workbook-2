6fa229f7986148c44682398030b5b3b283f81efc
Jesse Hernandez <101133566+jdez23@users.noreply.github.com>
2025-04-18 15:29:59 -0700

    init looping app

---
+++ LoopingExercises/src/main/java/com/pluralsight/DoWhileLoop.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int count = 0;
+        String text = "I love Java";
+
+        do {
+            count ++;
+            System.out.println("I love Java");
+        } while (count < 5);
+
+    }
+}
---
+++ LoopingExercises/src/main/java/com/pluralsight/WhileLoop.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int count = 0;
+        String text = "I love Java";
+
+        while (count < 5) {
+            System.out.println(text);
+            count ++;
+        }
+
+    }
+}
